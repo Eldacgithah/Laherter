0cc73826015cbf054f19903896a8ec8f926cab28
Eldacgithah <eeeldac@gmail.com>
2025-09-22 19:34:47 +0300

    Update main.py

--- legacy/main.py
+++ legacy/main.py
@@ -22,6 +22,7 @@
 # 🌐 https://github.com/hikariatama/Hikka
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
+#потсхалко
 
 import argparse
 import asyncio
